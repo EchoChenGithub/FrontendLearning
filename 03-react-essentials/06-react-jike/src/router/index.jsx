d4b47876acb58e13edf6fbafc451b24eac2fad47
Echo <chenqian_cq@foxmail.com>
2025-08-08 01:16:27 +0800

    feat(jike)：路由懒加载

--- 03-react-essentials/06-react-jike/src/router/index.jsx
+++ 03-react-essentials/06-react-jike/src/router/index.jsx
@@ -1,11 +1,19 @@
 // 用 react-router-dom 的 createBrowserRouter创建路由实例
-import {createBrowserRouter, Navigate} from "react-router-dom";
-import Layout from "@/pages/Layout";
+import {createBrowserRouter, Navigate} from "react-router-dom"
+import Layout from "@/pages/Layout"
 import Login from "@/pages/Login"
-import {getLocalToken} from "@/utils/index.jsx";
-import Home from "@/pages/Home/index.jsx";
-import Article from "@/pages/Article/index.jsx";
-import Publish from "@/pages/Publish/index.jsx";
+import {getLocalToken} from "@/utils/index.jsx"
+// import Home from "@/pages/Home/index.jsx"
+// import Article from "@/pages/Article/index.jsx"
+// import Publish from "@/pages/Publish/index.jsx"
+import {lazy, Suspense} from "react"
+
+
+// 1. lazy 函数对组件进行动态导入
+const Home = lazy(() => import("@/pages/Home/index.jsx"))
+const Article = lazy(() => import("@/pages/Article/index.jsx"))
+const Publish = lazy(() => import("@/pages/Publish/index.jsx"))
+
 
 // 定义 AuthGuard 函数：如果有 token，返回路由组件，如果没有则返回登录页面
 function AuthGuard({children}){
@@ -29,20 +37,26 @@ const router = createBrowserRouter([
         children:[
             {
                 path: "/",
-                element:(
-                    <Home />
+                element: (
+                    <Suspense fallback={<div>loading...</div>}>
+                        <Home />
+                    </Suspense>
                 )
             },
             {
                 path: "/publish",
                 element:(
-                    <Publish />
+                    <Suspense fallback={<div>loading...</div>}>
+                        <Publish />
+                    </Suspense>
                 )
             },
             {
                 path: "/article",
                 element:(
-                    <Article />
+                    <Suspense fallback={<div>loading...</div>}>
+                        <Article />
+                    </Suspense>
                 )
             }
         ]
